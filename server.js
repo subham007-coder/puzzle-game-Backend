d87cf17206addc851b0d421d26a43978f62e2943
subham007-coder <dsubham109@gmail.com>
2024-11-27 22:36:17 +0530

    addd

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log("Cloudinary Config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
