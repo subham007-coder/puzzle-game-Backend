849c3fcfad5295b4206d4998435245e90fbd2b88
subham007-coder <dsubham109@gmail.com>
2024-12-01 12:53:03 +0530

    add

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://f-end-puzzle-game-admin-panel.vercel.app'],
+  origin: ['https://f-end-puzzle-game-admin-panel.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
